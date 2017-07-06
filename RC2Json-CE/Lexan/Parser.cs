c22d0a90295671b7a7b344c55529b617561a2771
andrea-rinaldi-microarea <andrea.rinaldi@microarea.it>
2017-07-06 14:35:18 +0200

    aggiunta nota per licenza

--- RC2Json-CE/Lexan/Parser.cs
+++ RC2Json-CE/Lexan/Parser.cs
@@ -1,3 +1,18 @@
+/*
+RC2Json CE - Tool di trasformazione .RC in TB Json 
+Copyright (C) 2017 Microarea s.p.a.
+
+This program is free software: you can redistribute it and/or modify it under the 
+terms of the GNU General Public License as published by the Free Software Foundation, 
+either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+or FITNESS FOR A PARTICULAR PURPOSE. 
+
+See the GNU General Public License for more details.
+*/
+
 using System;
 using System.Collections;
 using System.Diagnostics;
--- RC2Json-CE/Structures.cs
+++ RC2Json-CE/Structures.cs
@@ -1,4 +1,19 @@
-﻿using Newtonsoft.Json;
+﻿/*
+RC2Json CE - Tool di trasformazione .RC in TB Json 
+Copyright (C) 2017 Microarea s.p.a.
+
+This program is free software: you can redistribute it and/or modify it under the 
+terms of the GNU General Public License as published by the Free Software Foundation, 
+either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY 
+or FITNESS FOR A PARTICULAR PURPOSE. 
+
+See the GNU General Public License for more details.
+*/
+
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
